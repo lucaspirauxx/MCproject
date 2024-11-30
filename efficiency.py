0def806695c7405e0890381ac21eb8a8ddf93117
lucaspirauxx <lucas.piraux@gmail.com>
2024-11-30 13:51:41 +0100

    nouveau fichier pour une épaisseur

--- efficiency.py
+++ efficiency.py
@@ -23,43 +23,15 @@ def transmission(thickness, sigma_a, sigma_s, neutrons) :
     return transmission_prob, accuracy
 
 # Variables
-thickness_list = np.linspace(0, 100, 100)   # For typical concrete wall (cm)
+thickness = 200   # For typical concrete wall (cm)
 sigma_a = 0.01  # Absorption for concrete wall  (cm-1)
 sigma_s = 0.4   # Scattering for concrete wall  (cm-1)
-neutrons = 1000
+neutrons = 10000
 
 # Call
-transmission_probability = []
-accuracy = []
-for thickness in thickness_list:
-    prob, acc = transmission(thickness, sigma_a, sigma_s, neutrons)
-    transmission_probability.append(prob)
-    accuracy.append(acc)
+prob, acc = transmission(thickness, sigma_a, sigma_s, neutrons)
 
-# sigma_a/sigma_t sensitivity
-ratio = [0.1, 0.3, 0.5, 1, 2, 3, 4]
-transmission_probability_ratio = []
-ratio_cross_section = []
-for i in ratio:
-    transmission_probability_ratio.append(transmission(50, sigma_a*i, sigma_s, neutrons)[0])
-    ratio_cross_section.append(sigma_a*i/sigma_s)
+print("Transmission probability =", prob)
+print("Accuracy =", acc)
 
-# Plot
-plt.figure(figsize=(12, 6))
 
-plt.subplot(2, 2, 1)
-plt.plot(thickness_list, transmission_probability)
-plt.xlabel('Thickness')
-plt.ylabel('Transmission probability')
-
-plt.subplot(2, 2, 2)
-plt.plot(thickness_list, accuracy)
-plt.xlabel('Thickness')
-plt.ylabel('Accuracy')
-
-plt.subplot(2, 2, 3)
-plt.plot(ratio_cross_section, transmission_probability_ratio)
-plt.xlabel('sigma_a/sigma_s')
-plt.ylabel('Transmission probability')
-
-plt.show()
